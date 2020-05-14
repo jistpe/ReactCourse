1b17b4d1bcefda9887485df89c2876f8b2491d80
jacó <jistpe@gmail.com>
2020-05-14 15:51:06 -0300

    finalizado sessão de css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import './App.css';
+
+import classes from './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`;
 
 class App extends Component  {
   state = { 
@@ -40,7 +26,7 @@ class App extends Component  {
 
   nameChangeHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
+      return p.userId === id;
     });
     const person = {...this.state.persons[personIndex]};
 
@@ -54,6 +40,7 @@ class App extends Component  {
 
   render() {
   let persons = null;
+  let btnClass = [classes.Button];
 
   if(this.state.showPersons){
     persons = (
@@ -67,21 +54,23 @@ class App extends Component  {
         })}
       </div>
     );
+
+    btnClass.push(classes.Red);
   }
 
-  let classes = [];
+  const assignedClasses = [];
   if(this.state.persons.length <= 2){
-    classes.push('red');
+    assignedClasses.push(classes.red);
   }
   if(this.state.persons.length <= 1){
-    classes.push('bold');
+    assignedClasses.push(classes.bold);
   }
 
   return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button className='button' alt={this.state.showPersons} onClick={ this.tooglePersonsHandler }>Toggle Name</button>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass.join(' ')} onClick={ this.tooglePersonsHandler }>Toggle Name</button>
         {persons}
 
       </div>
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,29 +1,14 @@
 import React from 'react';
-// import './Person.css';
-import styled from 'styled-components'
+import classes from './Person.css';
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`;
 
 const person = ( props ) => {
     return (
-    // <div className='Person' style={style}>  
-        <StyledDiv>
+    <div className={classes.person}>
         <p onClick={ props.click }>I'm { props.name } and I am { props.age }</p> 
         <p>{ props.children }</p>
         <input type="text" onChange={ props.changed } value={ props.name } />
-        </StyledDiv>
-    // </div>  
+    </div>  
     )
 };
 
